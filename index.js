8cf2204552ed51e281a0680b723303d5cf505ee5
Hector Leon Zarco Garcia <hezarco@gmail.com>
2015-11-29 14:56:20 +0100

    Fix env

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var restify = require('restify');
 var pg = require('pg');
 var Stat = require('./lib/stat');
-var env = require('./environment');
+var env = require('./lib/env');
 var server = restify.createServer({
   name: 'gitspy',
   version: '0.1.0'
@@ -23,18 +23,18 @@ server.get('/repos', function (req, res, next) {
   });
 });
 
+server.post('/repos/:repo_id', function(req, res) {
+  var repoId = req.params.repo_id;
+  // TODO: Sanetize data
+  res.send({repoId: repoId});
+});
+
 var client = new pg.Client(env.databaseUrl);
 
 client.connect(function(err) {
   if (err) {
     return console.error('could not connect to postgres', err);
   }
-
-  // client.query("INSERT INTO repository (user_id, name) VALUES ('zzarcon', 'focusable')");
-
-  client.query("select * from repository", function(err, result) {
-    console.log("result", result);
-  });
 });
 
 server.listen(env.port, function () {
